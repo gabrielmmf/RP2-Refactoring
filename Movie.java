9ac6174d198c713be53a6fb3caa5ce9e308150ce
gabrielmmf <gabrielmmfialho@hotmail.com>
2022-11-29 23:50:15 -0300

    Commit 12

--- Movie.java
+++ Movie.java
@@ -38,24 +38,7 @@ public class Movie {
 
     public double getCharge(int daysRented) {
         // Adicionar o trecho de código extraído.
-        double thisAmount = 0;
-        switch (_price.getPriceCode()) {
-            case Movie.REGULAR:
-                thisAmount += 2;
-                if (daysRented > 2)
-                    thisAmount += (daysRented - 2) * 1.5;
-                break;
-            case Movie.NEW_RELEASE:
-                thisAmount += daysRented * 3;
-                break;
-            case Movie.CHILDRENS:
-                thisAmount += 1.5;
-                if (daysRented > 3)
-                    thisAmount += (daysRented - 3) * 1.5;
-                break;
-        }
-
-        return thisAmount;
+        return _price.getCharge(daysRented);
     }
 
     public int getFrequentRenterPoints(int daysRented) {
