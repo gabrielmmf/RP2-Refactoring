38689a8f6bd721b71deb4fa74309b2466f83e013
gabrielmmf <gabrielmmfialho@hotmail.com>
2022-11-29 23:59:13 -0300

    Commit 15

--- Price.java
+++ Price.java
@@ -4,15 +4,6 @@ public abstract class Price {
     public abstract double getCharge(int daysRented);
 
     public int getFrequentRenterPoints(int daysRented) {
-        // Adicionar o trecho de código extraído.
-        int frequentRenterPoints = 0;
-        // add frequent renter points
-        frequentRenterPoints++;
-        // add bonus for a two day new release rental
-        if ((this.getPriceCode() == Movie.NEW_RELEASE) &&
-                daysRented > 1)
-            frequentRenterPoints++;
-
-        return frequentRenterPoints;
+        return 1;
     }
 }
