2b8c47c2519d297deed2ef88c1535f2988156a38
gabrielmmf <gabrielmmfialho@hotmail.com>
2022-11-29 23:56:00 -0300

    Commit 13

--- Price.java
+++ Price.java
@@ -1,25 +1,5 @@
 public abstract class Price {
     public abstract int getPriceCode();
 
-    public double getCharge(int daysRented) {
-        // Adicionar o trecho de código extraído.
-        double thisAmount = 0;
-        switch (this.getPriceCode()) {
-            case Movie.REGULAR:
-                thisAmount += 2;
-                if (daysRented > 2)
-                    thisAmount += (daysRented - 2) * 1.5;
-                break;
-            case Movie.NEW_RELEASE:
-                thisAmount += daysRented * 3;
-                break;
-            case Movie.CHILDRENS:
-                thisAmount += 1.5;
-                if (daysRented > 3)
-                    thisAmount += (daysRented - 3) * 1.5;
-                break;
-        }
-
-        return thisAmount;
-    }
+    public abstract double getCharge(int daysRented);
 }
