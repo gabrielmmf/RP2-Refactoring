a0efff8029500be0310dcefab74cf9c8fdc64f7f
gabrielmmf <gabrielmmfialho@hotmail.com>
2022-11-29 23:58:07 -0300

    Commit 14

--- Movie.java
+++ Movie.java
@@ -43,14 +43,6 @@ public class Movie {
 
     public int getFrequentRenterPoints(int daysRented) {
         // Adicionar o trecho de código extraído.
-        int frequentRenterPoints = 0;
-        // add frequent renter points
-        frequentRenterPoints++;
-        // add bonus for a two day new release rental
-        if ((_price.getPriceCode() == Movie.NEW_RELEASE) &&
-                daysRented > 1)
-            frequentRenterPoints++;
-
-        return frequentRenterPoints;
+        return _price.getFrequentRenterPoints(daysRented);
     }
 }
